fbb909dd5eb86c8db5691ea858eea13eb32520f6
Amauri Araujo <amauriaraujojr@yahoo.com.br>
2024-04-18 15:50:30 -0300

    feat: cards pers

--- src/components/CardList/Card/index.tsx
+++ src/components/CardList/Card/index.tsx
@@ -1,21 +1,32 @@
-import { TimeData} from "@/schemas/times.schema";
+import { TimeData } from "@/schemas/times.schema";
 import styles from "./styles.module.scss";
+import dot from "../../../assets/img/Group 2.png";
+import Image from "next/image";
 
-interface CardProps{
-  time:TimeData
+interface CardProps {
+  time: TimeData;
 }
 
-export const Card = (time:CardProps) => {
+export const Card = ({ time }: CardProps) => {
   return (
-    <li className={styles.container}>
-      <div> 
-        <h3>{time.time.title}</h3>
-      </div>
-      <p>{time.time.timeframes.weekly.current}hrs</p>
-      <div>
-        <p>Last Week</p>
-        <p>{time.time.timeframes.weekly.previous}hrs</p>
-      </div>
-    </li>
+    <div className={styles[`${time.title}`]}>
+      oi
+      <li className={styles.container}>
+        <div className={styles.box_title}>
+          {time.title == "Self" ? (
+            <h3>{time.title} Care</h3>
+          ) : (
+            <h3>{time.title}</h3>
+          )}
+          <Image src={dot} alt="..." />
+        </div>
+        <div className={styles.box_hour}>
+          <p>{time.timeframes.weekly.current}hrs</p>
+        </div>
+        <div className={styles.box_time}>
+          <p>Last Week - {time.timeframes.weekly.previous}hrs</p>
+        </div>
+      </li>
+    </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,132 @@
 import { CardList } from "@/components/CardList";
+import { TimeData } from "@/schemas/times.schema";
+import { GetServerSideProps, NextPage } from "next";
 import { Rubik } from "next/font/google";
 
-const rubik = Rubik({ 
-  weight:["300","400","500","600"],
-  subsets: ["latin"] });
-
-export default function Home() {
-  return (
-    <div className={rubik.className}>
-    <CardList/>
-     
-    </div>
-  );
+interface HomeProps {
+  times: TimeData[];
 }
+const rubik = Rubik({
+  weight: ["300", "400", "500", "600"],
+  subsets: ["latin"],
+});
+
+const Home: NextPage<HomeProps> = ({ times }) => {
+  return (
+    <main className={rubik.className}>
+      <CardList times={times} />
+    </main>
+  );
+};
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response: TimeData[] = [
+    {
+      title: "Work",
+      timeframes: {
+        daily: {
+          current: 5,
+          previous: 7,
+        },
+        weekly: {
+          current: 32,
+          previous: 36,
+        },
+        monthly: {
+          current: 103,
+          previous: 128,
+        },
+      },
+    },
+    {
+      title: "Play",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 2,
+        },
+        weekly: {
+          current: 10,
+          previous: 8,
+        },
+        monthly: {
+          current: 23,
+          previous: 29,
+        },
+      },
+    },
+    {
+      title: "Study",
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 7,
+        },
+        monthly: {
+          current: 13,
+          previous: 19,
+        },
+      },
+    },
+    {
+      title: "Exercise",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 1,
+        },
+        weekly: {
+          current: 4,
+          previous: 5,
+        },
+        monthly: {
+          current: 11,
+          previous: 18,
+        },
+      },
+    },
+    {
+      title: "Social",
+      timeframes: {
+        daily: {
+          current: 1,
+          previous: 3,
+        },
+        weekly: {
+          current: 5,
+          previous: 10,
+        },
+        monthly: {
+          current: 21,
+          previous: 23,
+        },
+      },
+    },
+    {
+      title:"Self",
+      timeframes: {
+        daily: {
+          current: 0,
+          previous: 1,
+        },
+        weekly: {
+          current: 2,
+          previous: 2,
+        },
+        monthly: {
+          current: 7,
+          previous: 11,
+        },
+      },
+    },
+  ];
+  return {
+    props: { times: response },
+  };
+};
+
+export default Home
