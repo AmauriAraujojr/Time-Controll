682e4d6a4d8d184c4599033de6c324ba8e19b1cb
Amauri Araujo <amauriaraujojr@yahoo.com.br>
2024-04-20 14:13:11 -0300

    feat: responsive

--- src/components/CardList/Card/index.tsx
+++ src/components/CardList/Card/index.tsx
@@ -2,12 +2,13 @@ import { TimeData } from "@/schemas/times.schema";
 import styles from "./styles.module.scss";
 import dot from "../../../assets/img/icon-ellipsis.svg";
 import Image from "next/image";
-import workIcon from "../../../assets/img/icon-work.svg"
-import playIcon from "../../../assets/img/icon-play.svg"
-import sutdyIcon from "../../../assets/img/icon-study.svg"
-import exerciceIcon from "../../../assets/img/icon-exercise.svg"
-import socialIcon from "../../../assets/img/icon-social.svg"
-import selfIcon from "../../../assets/img/icon-self-care.svg"
+import workIcon from "../../../assets/img/icon-work.svg";
+import playIcon from "../../../assets/img/icon-play.svg";
+import sutdyIcon from "../../../assets/img/icon-study.svg";
+import exerciceIcon from "../../../assets/img/icon-exercise.svg";
+import socialIcon from "../../../assets/img/icon-social.svg";
+import selfIcon from "../../../assets/img/icon-self-care.svg";
+import { symbol } from "zod";
 
 interface CardProps {
   time: TimeData;
@@ -15,13 +16,25 @@ interface CardProps {
 
 export const Card = ({ time }: CardProps) => {
   return (
-    <li className={styles[`${time.title}`]}>
-      {time.title == "Work"? <Image className={styles.icon} src={workIcon} alt="work"/>: null}
-      {time.title == "Play"? <Image  className={styles.icon}src={playIcon} alt="play"/>: null}
-      {time.title == "Study"? <Image className={styles.icon} src={sutdyIcon} alt="study"/>: null}
-      {time.title == "Exercise"? <Image className={styles.icon} src={exerciceIcon} alt="exercise"/>: null}
-      {time.title == "Social"? <Image className={styles.icon} src={socialIcon} alt="social"/>: null}
-      {time.title == "Self"? <Image className={styles.icon} src={selfIcon} alt="self"/>: null}
+    <li className={styles[`${time.title}`]} id={styles.box}>
+      {time.title == "Work" ? (
+        <Image className={styles.icon} src={workIcon} alt="work" />
+      ) : null}
+      {time.title == "Play" ? (
+        <Image className={styles.icon} src={playIcon} alt="play" />
+      ) : null}
+      {time.title == "Study" ? (
+        <Image className={styles.icon} src={sutdyIcon} alt="study" />
+      ) : null}
+      {time.title == "Exercise" ? (
+        <Image className={styles.icon} src={exerciceIcon} alt="exercise" />
+      ) : null}
+      {time.title == "Social" ? (
+        <Image className={styles.icon} src={socialIcon} alt="social" />
+      ) : null}
+      {time.title == "Self" ? (
+        <Image className={styles.icon} src={selfIcon} alt="self" />
+      ) : null}
 
       <div className={styles.container}>
         <div className={styles.box_title}>
@@ -32,11 +45,12 @@ export const Card = ({ time }: CardProps) => {
           )}
           <Image src={dot} alt="..." />
         </div>
+
         <div className={styles.box_hour}>
-          <p>{time.timeframes.weekly.current}hrs</p>
-        </div>
-        <div className={styles.box_time}>
-          <p>Last Week - {time.timeframes.weekly.previous}hrs</p>
+          <p className={styles.time}>{time.timeframes.weekly.current}hrs</p>
+          <p className={styles.last}>
+            Last Week - {time.timeframes.weekly.previous}hrs
+          </p>
         </div>
       </div>
     </li>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { CardList } from "@/components/CardList";
 import { TimeData } from "@/schemas/times.schema";
-import { GetServerSideProps, NextPage } from "next";
+import { GetServerSideProps, GetStaticProps, NextPage } from "next";
 import { Rubik } from "next/font/google";
 
 interface HomeProps {
@@ -19,114 +19,224 @@ const Home: NextPage<HomeProps> = ({ times }) => {
   );
 };
 
-export const getServerSideProps: GetServerSideProps = async () => {
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const response: TimeData[] = [
+//     {
+//       title: "Work",
+//       timeframes: {
+//         daily: {
+//           current: 5,
+//           previous: 7,
+//         },
+//         weekly: {
+//           current: 32,
+//           previous: 36,
+//         },
+//         monthly: {
+//           current: 103,
+//           previous: 128,
+//         },
+//       },
+//     },
+//     {
+//       title: "Play",
+//       timeframes: {
+//         daily: {
+//           current: 1,
+//           previous: 2,
+//         },
+//         weekly: {
+//           current: 10,
+//           previous: 8,
+//         },
+//         monthly: {
+//           current: 23,
+//           previous: 29,
+//         },
+//       },
+//     },
+//     {
+//       title: "Study",
+//       timeframes: {
+//         daily: {
+//           current: 0,
+//           previous: 1,
+//         },
+//         weekly: {
+//           current: 4,
+//           previous: 7,
+//         },
+//         monthly: {
+//           current: 13,
+//           previous: 19,
+//         },
+//       },
+//     },
+//     {
+//       title: "Exercise",
+//       timeframes: {
+//         daily: {
+//           current: 1,
+//           previous: 1,
+//         },
+//         weekly: {
+//           current: 4,
+//           previous: 5,
+//         },
+//         monthly: {
+//           current: 11,
+//           previous: 18,
+//         },
+//       },
+//     },
+//     {
+//       title: "Social",
+//       timeframes: {
+//         daily: {
+//           current: 1,
+//           previous: 3,
+//         },
+//         weekly: {
+//           current: 5,
+//           previous: 10,
+//         },
+//         monthly: {
+//           current: 21,
+//           previous: 23,
+//         },
+//       },
+//     },
+//     {
+//       title:"Self",
+//       timeframes: {
+//         daily: {
+//           current: 0,
+//           previous: 1,
+//         },
+//         weekly: {
+//           current: 2,
+//           previous: 2,
+//         },
+//         monthly: {
+//           current: 7,
+//           previous: 11,
+//         },
+//       },
+//     },
+//   ];
+//   return {
+//     props: { times: response },
+//   };
+// };
+
+export const getStaticProps:GetStaticProps= async()=>{
   const response: TimeData[] = [
-    {
-      title: "Work",
-      timeframes: {
-        daily: {
-          current: 5,
-          previous: 7,
+        {
+          title: "Work",
+          timeframes: {
+            daily: {
+              current: 5,
+              previous: 7,
+            },
+            weekly: {
+              current: 32,
+              previous: 36,
+            },
+            monthly: {
+              current: 103,
+              previous: 128,
+            },
+          },
         },
-        weekly: {
-          current: 32,
-          previous: 36,
+        {
+          title: "Play",
+          timeframes: {
+            daily: {
+              current: 1,
+              previous: 2,
+            },
+            weekly: {
+              current: 10,
+              previous: 8,
+            },
+            monthly: {
+              current: 23,
+              previous: 29,
+            },
+          },
         },
-        monthly: {
-          current: 103,
-          previous: 128,
+        {
+          title: "Study",
+          timeframes: {
+            daily: {
+              current: 0,
+              previous: 1,
+            },
+            weekly: {
+              current: 4,
+              previous: 7,
+            },
+            monthly: {
+              current: 13,
+              previous: 19,
+            },
+          },
         },
-      },
-    },
-    {
-      title: "Play",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 2,
+        {
+          title: "Exercise",
+          timeframes: {
+            daily: {
+              current: 1,
+              previous: 1,
+            },
+            weekly: {
+              current: 4,
+              previous: 5,
+            },
+            monthly: {
+              current: 11,
+              previous: 18,
+            },
+          },
         },
-        weekly: {
-          current: 10,
-          previous: 8,
+        {
+          title: "Social",
+          timeframes: {
+            daily: {
+              current: 1,
+              previous: 3,
+            },
+            weekly: {
+              current: 5,
+              previous: 10,
+            },
+            monthly: {
+              current: 21,
+              previous: 23,
+            },
+          },
         },
-        monthly: {
-          current: 23,
-          previous: 29,
+        {
+          title:"Self",
+          timeframes: {
+            daily: {
+              current: 0,
+              previous: 1,
+            },
+            weekly: {
+              current: 2,
+              previous: 2,
+            },
+            monthly: {
+              current: 7,
+              previous: 11,
+            },
+          },
         },
-      },
-    },
-    {
-      title: "Study",
-      timeframes: {
-        daily: {
-          current: 0,
-          previous: 1,
-        },
-        weekly: {
-          current: 4,
-          previous: 7,
-        },
-        monthly: {
-          current: 13,
-          previous: 19,
-        },
-      },
-    },
-    {
-      title: "Exercise",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 1,
-        },
-        weekly: {
-          current: 4,
-          previous: 5,
-        },
-        monthly: {
-          current: 11,
-          previous: 18,
-        },
-      },
-    },
-    {
-      title: "Social",
-      timeframes: {
-        daily: {
-          current: 1,
-          previous: 3,
-        },
-        weekly: {
-          current: 5,
-          previous: 10,
-        },
-        monthly: {
-          current: 21,
-          previous: 23,
-        },
-      },
-    },
-    {
-      title:"Self",
-      timeframes: {
-        daily: {
-          current: 0,
-          previous: 1,
-        },
-        weekly: {
-          current: 2,
-          previous: 2,
-        },
-        monthly: {
-          current: 7,
-          previous: 11,
-        },
-      },
-    },
-  ];
+      ];
   return {
-    props: { times: response },
-  };
-};
+    props:{times:response },revalidate:60}
+  
+}
 
 export default Home
